a7fde930efe9949550fbf3b135ba7355aa5b4673
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-08-23 21:30:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	let num = 1;
+	for(let i = 1;i<=n;i++) {
+		num*=i;
+	}
+	let s = num+"";
+	let ct = 0;
+	for(let i = s.length-1;i>=0;i--) {
+		if(s.charAt(i)!='0') {
+			return ct;
+		}
+		ct++;
+	}
+	return ct;
 }
 
 const input = prompt("Enter a number");
